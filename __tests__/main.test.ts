0a48ec44fed52128a8f08d7cf5a822b7c438f0c8
mie035 <hello@michitoshiyano.com>
2024-09-13 01:12:20 +0900

    chore

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -13,26 +13,17 @@ import * as XAPI from '../src/x'
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
-
 // Mock the GitHub Actions core library
-let debugMock: jest.SpiedFunction<typeof core.debug>
 let errorMock: jest.SpiedFunction<typeof core.error>
 let getInputMock: jest.SpiedFunction<typeof core.getInput>
-let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
-let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
 let xMock: jest.SpiedFunction<typeof XAPI.X>
 
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
-    debugMock = jest.spyOn(core, 'debug').mockImplementation()
     errorMock = jest.spyOn(core, 'error').mockImplementation()
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
     xMock = jest.spyOn(XAPI, 'X').mockImplementation()
   })
 
@@ -50,8 +41,13 @@ describe('action', () => {
     await main.run()
     expect(runMock).toHaveReturned()
 
-    expect(xMock).toHaveBeenCalledWith( {"accessSecret": "", "accessToken": "", "appKey": "", "appSecret": ""})
-    
+    expect(xMock).toHaveBeenCalledWith({
+      accessSecret: '',
+      accessToken: '',
+      appKey: '',
+      appSecret: ''
+    })
+
     expect(errorMock).not.toHaveBeenCalled()
   })
 })
