472017c10155c01413274d79de8036a785d4711b
mie035 <hello@michitoshiyano.com>
2024-09-13 00:50:42 +0900

    mod x

---
+++ src/x.ts
@@ -0,0 +1,14 @@
+import { TwitterApi } from 'twitter-api-v2';
+
+type Credentials = {
+    appKey:string;
+    appSecret:string;
+    accessToken:string;
+    accessSecret:string;
+}
+export const X = (credentials: Credentials) => {
+    const api = new TwitterApi(credentials);
+    return (message: string) => {
+        return api.v2.tweet(message);
+    }
+}
