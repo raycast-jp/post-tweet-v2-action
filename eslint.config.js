b9a7629e241b9755cd42aae337a4afa7473ebf21
mie035 <hello@michitoshiyano.com>
2025-05-06 22:15:37 +0900

    feat: mig eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import jestPlugin from 'eslint-plugin-jest';
+
+export default [
+  eslint.configs.recommended,
+  {
+    ignores: [
+      '**/node_modules/*',
+      '**/dist/*',
+      '**/coverage/*',
+      '*.json'
+    ]
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        project: [
+          './.github/linters/tsconfig.json',
+          './tsconfig.json'
+        ]
+      },
+      globals: {
+        ...jestPlugin.environments.globals.globals
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'jest': jestPlugin
+    },
+    rules: {
+      'camelcase': 'off',
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'semi': 'off',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      '@typescript-eslint/explicit-member-accessibility': ['error', { 'accessibility': 'no-public' }],
+      '@typescript-eslint/explicit-function-return-type': ['error', { 'allowExpressions': true }],
+      '@typescript-eslint/no-array-constructor': 'error',
+      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-extraneous-class': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-for-in-array': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/no-misused-new': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/no-unnecessary-qualifier': 'error',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-useless-constructor': 'error',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/prefer-for-of': 'warn',
+      '@typescript-eslint/prefer-function-type': 'warn',
+      '@typescript-eslint/prefer-includes': 'error',
+      '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+      '@typescript-eslint/promise-function-async': 'error',
+      '@typescript-eslint/require-array-sort-compare': 'error',
+      '@typescript-eslint/restrict-plus-operands': 'error',
+      '@typescript-eslint/space-before-function-paren': 'off',
+      '@typescript-eslint/unbound-method': 'error'
+    }
+  },
+  {
+    files: ['**/__tests__/**/*.ts'],
+    languageOptions: {
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off'
+    }
+  }
+]; 
