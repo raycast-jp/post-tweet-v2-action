6f8f8363e77b2bd1ba939ed9cdedab59bb89cd0e
mie035 <hello@michitoshiyano.com>
2025-05-07 04:45:01 +0900

    chore

--- dist/index.js
+++ dist/index.js
@@ -34645,7 +34645,7 @@ async function run() {
             accessToken: core.getInput('access-token'),
             accessSecret: core.getInput('access-token-secret')
         });
-        const result = await x(core.getInput('message'));
+        const result = await x(core.getInput('message'), core.getInput('image'));
         core.setOutput('tweetID', result.data.id);
     }
     catch (error) {
